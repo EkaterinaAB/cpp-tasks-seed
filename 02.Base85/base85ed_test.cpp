d03d2648a2bf0458b916b608d3c1a2acbf83a7b6
EkaterinaAB <134541902+EkaterinaAB@users.noreply.github.com>
2026-05-16 13:49:28 -0700

    add tests

--- 02.Base85/base85ed_test.cpp
+++ 02.Base85/base85ed_test.cpp
@@ -43,3 +43,69 @@ TEST(Base85ShortsDecode, TrivialShortDecodes)
         EXPECT_EQ(base85::decode(cstr2v(p.first)), cstr2v(p.second));
     }
 }
+
+const std::vector<std::pair<const char*, const char*>> long_cases =
+{
+    { "F)}kWH8wXmIR",     "123456789"  },
+    { "F)}kWH8wXmIWP",    "1234567890" },
+    { "NM&qnZ!92pZ*pv8Ap", "Hello, world!" },
+    { "VPRomVPRomVPO",    "aaaaaaaaaa" },
+    { "WM*|?Xm(_4YG`(8WjHf3IXEgNWNU6_M@I",    "dfuahvdkjhvie83298*&dknfGG" },
+};
+
+
+TEST(Base85LongEncode, LongEncodes)
+{
+    for (const auto &p : long_cases)
+    {
+        EXPECT_EQ(base85::encode(cstr2v(p.second)), cstr2v(p.first));
+    }
+}
+
+TEST(Base85LongDecode, LongDecodes)
+{
+    for (const auto &p : long_cases)
+    {
+        EXPECT_EQ(base85::decode(cstr2v(p.first)), cstr2v(p.second));
+    }
+}
+
+TEST(Base85DecodeEncodeShort, RoundShort_1)
+{
+    for (const auto &p : short_cases)
+    {
+        auto true_word = cstr2v(p.second);
+        auto result = base85::decode(base85::encode(true_word));
+        EXPECT_EQ(result, true_word);
+    }
+}
+
+TEST(Base85DecodeEncodeShort, RoundShort_2)
+{
+    for (const auto &p : short_cases)
+    {
+        auto true_word = cstr2v(p.first);
+        auto result = base85::encode(base85::decode(true_word));
+        EXPECT_EQ(result, true_word);
+    }
+}
+
+TEST(Base85DecodeEncodeLong, RoundLong_1)
+{
+    for (const auto &p : long_cases)
+    {
+        auto true_word = cstr2v(p.second);
+        auto result = base85::decode(base85::encode(true_word));
+        EXPECT_EQ(result, true_word);
+    }
+}
+
+TEST(Base85DecodeEncodeLong, RoundLong_2)
+{
+    for (const auto &p : long_cases)
+    {
+        auto true_word = cstr2v(p.first);
+        auto result = base85::encode(base85::decode(true_word));
+        EXPECT_EQ(result, true_word);
+    }
+}
